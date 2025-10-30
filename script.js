ccf84f3e43ff9e39bfd2961c589cb8ab6c704ae0
Arthur-v-2006 <arthur.vinicius.l.2006@gmail.com>
2025-10-30 04:03:48 -0300

    Fazendo uma alteração no js

--- script.js
+++ script.js
@@ -97,7 +97,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 const nome = card.querySelector('h4').textContent;
                 const pais = card.getAttribute('data-pais');
                 const titulos = card.getAttribute('data-titulos')?.split(';') || [];
-                const curiosidade = card.getAttribute('data-curiosidade');
+                const curiosidades = card.getAttribute('data-curiosidade')?.split(';') || [];
                 
                 modalTimeBody.innerHTML = `
                     <h2>${nome}</h2>
@@ -105,7 +105,7 @@ document.addEventListener('DOMContentLoaded', function() {
                     <h3>Principais Títulos:</h3>
                     <ul>${titulos.map(t => `<li>${t.trim()}</li>`).join('')}</ul>
                     <h3>Curiosidade:</h3>
-                    <p>${curiosidade}</p>
+                    <ul>${curiosidades.map(c => `<li>${c.trim()}</li>`).join('')}</ul>
                 `;
                 modalTime.style.display = 'flex';
                 document.body.style.overflow = 'hidden';
